ba53253d59004d2c717d6097ba1a79d8edfc191d
Thomas Guyard <t.guyard@gigatribe.com>
2016-03-04 16:18:12 +0100

    update deps

--- vendors/VS2015/copydep.bat
+++ vendors/VS2015/copydep.bat
@@ -2,8 +2,8 @@ REM boost
 for /f %%f in ('dir /b %1\packages\boost_*') do xcopy %1packages\%%f\lib\native\address-model-32\lib %1deps\Debug\lib /y /s
 for /f %%f in ('dir /b %1\packages\boost_*') do xcopy %1packages\%%f\lib\native\address-model-32\lib %1deps\Release\lib /y /s
 
-for /f %%f in ('dir /b %1\packages\boost_*') do xcopy %1packages\%%f\lib\native\address-model-32\lib\*.dll %1deps\Debug\bin /y /s
-for /f %%f in ('dir /b %1\packages\boost_*') do xcopy %1packages\%%f\lib\native\address-model-32\lib\*.dll %1deps\Release\bin /y /s
+REM for /f %%f in ('dir /b %1\packages\boost_*') do xcopy %1packages\%%f\lib\native\address-model-32\lib\*.dll %1deps\Debug\bin /y /s
+REM for /f %%f in ('dir /b %1\packages\boost_*') do xcopy %1packages\%%f\lib\native\address-model-32\lib\*.dll %1deps\Release\bin /y /s
 
 for /f %%f in ('dir /b %1\packages\boost*') do xcopy %1packages\%%f\lib\native\include %1deps\include /y /s
 
@@ -17,15 +17,15 @@ for /f %%f in ('dir /b %1\packages\cpprestsdk.v140.windesktop.msvcstl.dyn.rt-dyn
 for /f %%f in ('dir /b %1\packages\cpprestsdk.v140.windesktop.msvcstl.dyn.rt-dyn*') do xcopy %1packages\%%f\build\native\include %1deps\include /y /s
 
 REM cryptopp
-for /f %%f in ('dir /b %1\packages\cryptopp.v140.windesktop.msvcstl.dyn.rt-dyn-Win32*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\Win32\Debug\md   %1deps\Debug\lib /y /s
-for /f %%f in ('dir /b %1\packages\cryptopp.v140.windesktop.msvcstl.dyn.rt-dyn-Win32*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\Win32\Release\md %1deps\Release\lib /y /s
-for /f %%f in ('dir /b %1\packages\cryptopp.v140.windesktop.msvcstl.dyn.rt-dyn-Win32*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\Win32\Debug      %1deps\Debug\lib /y /s
-for /f %%f in ('dir /b %1\packages\cryptopp.v140.windesktop.msvcstl.dyn.rt-dyn-Win32*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\Win32\Release    %1deps\Release\lib /y /s
+REM for /f %%f in ('dir /b %1\packages\cryptopp.v140.windesktop.msvcstl.dyn.rt-dyn-Win32*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\Win32\Debug\md   %1deps\Debug\lib /y /s
+REM for /f %%f in ('dir /b %1\packages\cryptopp.v140.windesktop.msvcstl.dyn.rt-dyn-Win32*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\Win32\Release\md %1deps\Release\lib /y /s
+REM for /f %%f in ('dir /b %1\packages\cryptopp.v140.windesktop.msvcstl.dyn.rt-dyn-Win32*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\Win32\Debug      %1deps\Debug\lib /y /s
+REM for /f %%f in ('dir /b %1\packages\cryptopp.v140.windesktop.msvcstl.dyn.rt-dyn-Win32*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\Win32\Release    %1deps\Release\lib /y /s
 
-for /f %%f in ('dir /b %1\packages\cryptopp.v140.windesktop.msvcstl.dyn.rt-dyn-Win32*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\Win32\Debug\md\*.dll   %1deps\Debug\bin /y /s
-for /f %%f in ('dir /b %1\packages\cryptopp.v140.windesktop.msvcstl.dyn.rt-dyn-Win32*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\Win32\Release\md\*.dll %1deps\Release\bin /y /s
+REM for /f %%f in ('dir /b %1\packages\cryptopp.v140.windesktop.msvcstl.dyn.rt-dyn-Win32*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\Win32\Debug\md\*.dll   %1deps\Debug\bin /y /s
+REM for /f %%f in ('dir /b %1\packages\cryptopp.v140.windesktop.msvcstl.dyn.rt-dyn-Win32*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\Win32\Release\md\*.dll %1deps\Release\bin /y /s
 
-for /f %%f in ('dir /b %1\packages\cryptopp.5*') do xcopy %1packages\%%f\build\native\include %1deps\include /y /s
+REM for /f %%f in ('dir /b %1\packages\cryptopp.5*') do xcopy %1packages\%%f\build\native\include %1deps\include /y /s
 
 REM openssl
 for /f %%f in ('dir /b %1\packages\openssl.v140.windesktop.msvcstl.dyn.rt-dyn.x86*') do xcopy %1packages\%%f\lib\native\v140\windesktop\msvcstl\dyn\rt-dyn\x86\debug   %1deps\Debug\lib /y /s
